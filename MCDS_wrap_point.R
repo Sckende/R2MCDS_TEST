be7030050f1fe6d028271a2639c4c57c3c8916d3
Sckende <clrccljuhasz@gmail.com>
2019-03-21 15:00:14 -0400

    Correction faute d'orthographe dans un message d'erreur

--- MCDS_wrap_point.R
+++ MCDS_wrap_point.R
@@ -207,7 +207,7 @@ mcds.wrap.point <-
     
     if(toupper(units$Length_units)%in%c("CENTIMETERS", "METERS", "KILOMETERS", "MILES", 
                                         "INCHES", "FEET", "YARDS", "NAUTICAL MILES")==FALSE)
-      stop("Distance units must be one of thse: 'Centimeters', 'Meters', 'Kilometers', 'Miles', 'Inches', 
+      stop("Distance units must be one of these: 'Centimeters', 'Meters', 'Kilometers', 'Miles', 'Inches', 
            'Feet', 'Yards', Or 'Nautical Miles'")  
     
     if(toupper(units$Distance_units)%in%c("CENTIMETERS", "METERS", "KILOMETERS", "MILES", 
